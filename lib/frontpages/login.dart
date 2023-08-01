c6574fba695fc83268638f2c911a54e81258feb2
arielmatius <arielmatius21@gmail.com>
2023-08-01 11:28:19 +0700

    added loginscreen placeholder

---
+++ lib/frontpages/login.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+        elevation: 0,
+      ),
+      body: Container(
+        child: Column(),
+      ),
+    );
+  }
+}
