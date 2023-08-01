c0bf905ea35c1a0f0633e9660584431d08f3b203
arielmatius <arielmatius21@gmail.com>
2023-08-01 12:39:22 +0700

    update login screen UI

--- lib/frontpages/login.dart
+++ lib/frontpages/login.dart
@@ -1,17 +1,144 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({super.key});
 
   @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Login"),
-        elevation: 0,
-      ),
-      body: Container(
-        child: Column(),
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Color(0xFF1D2939),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Selamat Datang!",
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white),
+              ),
+              Text(
+                "Silahkan Masuk",
+                style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 30,
+                    color: Colors.white),
+              ),
+              Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 316,
+                  child: Image.asset('figma/login_pic.png')),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 200,
+                child: Column(
+                  children: [
+                    // Buku Elektronik USU
+                    Column(
+                      children: [
+                        // email
+                        Container(
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(8))),
+                          height: 36,
+                          width: 295,
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  hintText: "NIM/NIP/USU's email",
+                                  border: InputBorder.none),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        // password
+                        Container(
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(8))),
+                          height: 36,
+                          width: 295,
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  hintText: "Password",
+                                  border: InputBorder.none),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    // Masuk button
+                    Container(
+                      width: 295,
+                      height: 32,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Color(0xFFf04437),
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(8)))),
+                        onPressed: () {
+                          setState(() {
+                            // Navigator.pushReplacementNamed(context, '/beranda');
+                          });
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Masuk",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500, fontSize: 16),
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Icon(
+                              Icons.arrow_forward,
+                              size: 20,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/beranda');
+                        },
+                        child: Text(
+                          'Lanjutkan tanpa akun.',
+                          style: TextStyle(color: Color(0xFFFEDF89)),
+                        ))
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
