10533632a82d1fc02f078e2fe64d0e1bfe32c6da
Ariel <86115219+MattRiel@users.noreply.github.com>
2023-12-06 11:15:06 +0700

    style (profile) : Update profile screen style

--- lib/src/features/profile/widgets/profile_btn_widget.dart
+++ lib/src/features/profile/widgets/profile_btn_widget.dart
@@ -1,4 +1,3 @@
-import 'package:bookstore/src/features/profile/theme/profile_text_style.dart';
 import 'package:flutter/material.dart';
 
 class ProfileBtn extends StatelessWidget {
@@ -15,24 +14,19 @@ class ProfileBtn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // var isDark = MediaQuery.of(context).platformBrightness == Brightness.dark;
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         borderRadius: BorderRadius.all(Radius.circular(4)),
-        color: Colors.white,
+        // color: isDark ? Colors.grey : Colors.white,
       ),
       height: 44,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              text,
-              style: ProfileTextStyle.profileText,
-            ),
-            IconButton(onPressed: onPressed, icon: Icon(icon)),
-          ],
-        ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(text, style: Theme.of(context).textTheme.bodyLarge),
+          IconButton(onPressed: onPressed, icon: Icon(icon)),
+        ],
       ),
     );
   }
